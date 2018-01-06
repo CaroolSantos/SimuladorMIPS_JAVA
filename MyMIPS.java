066ba8982614eb39be13aa5cc0d6731d86cbd33a
Ana Carolina <a.caroolinasantos@gmail.com>
2018-01-06 18:13:30 -0300

    Instruções MIPS

--- MyMIPS.java
+++ MyMIPS.java
@@ -1,30 +1,4 @@
 
-/*------------------------------------------------------------------------------
- *
- *   UNIVERSIDADE FEDERAL RURAL DE PERNAMBUCO - UFRPE (www.ufrpe.br)
- *   DEPARTAMENTO DE ESTATÍSTICA E INFORMÁTICA - DEINFO (www.deinfo.ufrpe.br)
- *
- *------------------------------------------------------------------------------
- *   Exemplo de utilização do Simulador.
- *
- *   Copyright (C) 2015  André Aziz Camilo de Araujo (andreaziz@deinfo.ufrpe.br)
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
- *------------------------------------------------------------------------------
- */
-
-
 import java.io.IOException;
 
 import jline.console.ConsoleReader;
